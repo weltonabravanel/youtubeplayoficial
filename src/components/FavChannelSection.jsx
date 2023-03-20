0128f29724a93bf0829e578a07b3c32f2011f94f
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-20 15:48:02 -0300

    Update FavChannelSection.jsx

--- src/components/FavChannelSection.jsx
+++ src/components/FavChannelSection.jsx
@@ -6,12 +6,17 @@ const FavChannelSection = () => {
     <div className="favchannel  flex  items-center gap-4 h-full pb-1 md:border-l border-gray-200/50 md:pl-10 pl-4">
       <h1 
       className='font-semibold text-sm pr-5'>Canais em Destaque</h1>
+    <FavChannel channelId="UC6rwiIxv0w2fbmmr66wl1rA" channelThumbnail="https://yt3.googleusercontent.com/l_VVAmpZd0AyqTWTgb7B9wlKEjxgbp1_Zelyv6T5yWZX7zws5j66fsK1xxPWW-hiS1BGQKBr=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
+    <FavChannel channelId="UC11JFCoslUUMTuAsUPH8RdQ" channelThumbnail="https://yt3.ggpht.com/AN17UxzyA-NjCh24gII5wH4eHFTDiGNwZi5LwFW7aoUxl105mtrhQ99L3r_yPD1HlxdhboHJH7U=s176-c-k-c0x00ffffff-no-rj-mo" handleClick="" />
+    <FavChannel channelId="UC376n347Ob5Lwzq2WGzF1AA" channelThumbnail="https://yt3.googleusercontent.com/nMGmGAtUOjZOwh4m8d-U80I-WkvplW7ez8NbhpUtaS3GAuRRFDZO2hgIj17TITrkxHnjYiD-6A=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
+    <FavChannel channelId="UCuiLR4p6wQ3xLEm15pEn1Xw" channelThumbnail="https://yt3.ggpht.com/xoqgjnZ9Wd8-cPvKryCyTcfeaT7K3rZtQxCJHhcbICXoVvw7x3hYcQ0S02kzvdmsqwq-Fgnt=s48-c-k-c0x00ffffff-no-rj" handleClick="" />
+    <FavChannel channelId="UCvdwhh_fDyWccR42-rReZLw" channelThumbnail="https://yt3.ggpht.com/ytc/AL5GRJVE5JVBS2la7rJ9hlAKLbPAaqZ8HE0EFXu8sYg7pQ=s48-c-k-c0x00ffffff-no-rj" handleClick="" />
     <FavChannel channelId="UCNJcSUSzUeFm8W9P7UUlSeQ" channelThumbnail="https://yt3.googleusercontent.com/ytc/AL5GRJUXTpfOB3f9ZiAw1bX9w2MkfO24C7J9SbBkqiS1Lw=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
     <FavChannel channelId="UCMiJRAwDNSNzuYeN2uWa0pA" channelThumbnail="https://yt3.googleusercontent.com/Ikb1C4ih2VMvfjma8OO5b39JnHL2CQcQgksB_I7TM-gGA3ERTY589OeLKCYyRQQO0nkE54-f=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
     <FavChannel channelId="UCAov2BBv1ZJav0c_yHEciAw" channelThumbnail="https://yt3.googleusercontent.com/ytc/AL5GRJVMk-lx1UVNsFd_zmdG15MDfbJXMBslqi17jmJX9g=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
     <FavChannel channelId="UC0rE2qq81of4fojo-KhO5rg" channelThumbnail="https://yt3.googleusercontent.com/ytc/AL5GRJWrmnbivN3S96UAKtX5YJFUAJNbqOnNCZEiaF_Pag=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
     <FavChannel channelId="UCRijo3ddMTht_IHyNSNXpNQ" channelThumbnail="https://yt3.googleusercontent.com/ytc/AL5GRJVdEnUyNNPVUQzeUAB0EFmP_cNCJWUX9qnBwAdesA=s176-c-k-c0x00ffffff-no-rj" handleClick="" />
-  
+    
   </div>
   )
 }
